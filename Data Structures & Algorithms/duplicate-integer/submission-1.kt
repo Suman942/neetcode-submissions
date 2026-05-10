240ffaca4ca69d64df7778a1b894ea9bfbb342fc
Suman Shil <54026457+Suman942@users.noreply.github.com>
2026-05-10 09:39:33 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun hasDuplicate(nums: IntArray): Boolean {
+        val set = mutableSetOf<Int>()
+
+        for(i in nums){
+            if(set.contains(i)) return true
+            set.add(i)
+        }
+        return false
+    }
+}
