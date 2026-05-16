15fda81af6a6578fe9a436f41db891ec0a166764
Suman Shil <54026457+Suman942@users.noreply.github.com>
2026-05-16 08:05:37 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.kt
@@ -0,0 +1,30 @@
+class Solution {
+    fun isValid(s: String): Boolean {
+        val map = mapOf(
+            ')' to '(',
+            '}' to '{',
+            ']' to '['
+        )
+        val stack = Stack<Char>()
+
+        for(ch in s){
+            when(ch){
+                '(' -> stack.push(ch)
+                '{' -> stack.push(ch)
+                '[' -> stack.push(ch)
+                else->{
+                    if(stack.isEmpty()){
+                        return false
+                    }
+
+                    val top = stack.pop()
+                    if(top != map[ch]!!){
+                        return false
+                    }
+                }
+
+            }
+        }
+        return stack.isEmpty()
+    }
+}
