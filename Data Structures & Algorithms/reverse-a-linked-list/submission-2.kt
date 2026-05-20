9f2de7d49ee76f3426f6661adff3cde8bf0a2f83
Suman Shil <54026457+Suman942@users.noreply.github.com>
2026-05-20 11:25:24 +0530

    Add: reverse-a-linked-list - submission-2

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-2.kt
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun reverseList(head: ListNode?): ListNode? {
+        var current = head
+        var prev : ListNode? = null
+
+        while(current != null){
+            val next = current.next
+            current.next = prev
+
+            prev = current
+            current = next
+        }
+
+        return prev
+    }
+}
