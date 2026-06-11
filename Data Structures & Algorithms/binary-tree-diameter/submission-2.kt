66821b0596a6880a746f6b77dca579ce6993ebda
Suman Shil <54026457+Suman942@users.noreply.github.com>
2026-06-11 08:40:42 +0530

    Add: binary-tree-diameter - submission-2

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-2.kt
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    var answer = 0
+    fun diameterOfBinaryTree(root: TreeNode?): Int {
+        if(root == null) return 0
+        // val lD = diameterOfBinaryTree(root.left)
+        // val rD = diameterOfBinaryTree(root.right)
+        // val cH = height(root.left) + height(root.right)
+        height(root)
+       return answer
+    }
+
+    fun height(root:TreeNode?):Int{
+        if(root == null) return 0
+
+        val l = height(root.left)
+        val r = height(root.right)
+        answer = maxOf((l+r),answer)
+        return maxOf(l,r) + 1
+    }
+}
