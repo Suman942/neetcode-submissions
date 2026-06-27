b8198fbc694bc760dbecbd23946f891657c3a033
Suman Shil <54026457+Suman942@users.noreply.github.com>
2026-06-27 09:36:23 +0530

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.kt
@@ -0,0 +1,29 @@
+class Solution {
+    fun characterReplacement(s: String, k: Int): Int {
+        val arr = IntArray(26)
+        var left = 0
+        var maxFreq = 0
+        var result = 0
+
+        for(right in s.indices){
+            // store frequency of new character
+           arr[s[right] - 'A']++ 
+            // store the max frequency of new character
+            maxFreq = maxOf(maxFreq,arr[s[right] - 'A'])
+            // window size
+            var windowSize = right - left + 1
+            /* 
+            1. how many replacement needed = windowSize - frequency
+            2. if replacement is greater than "K" then shrink window from left
+            */
+            if(windowSize - maxFreq > k){
+                arr[s[left] - 'A']--
+                left++
+            }
+            // record the maximum window valid size
+            result = maxOf(result,right - left + 1)
+        }
+
+        return result
+    }
+}
