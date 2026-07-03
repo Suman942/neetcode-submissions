857636c4ac7e35aee7a9013dd51d64b539d80bde
Suman Shil <54026457+Suman942@users.noreply.github.com>
2026-07-03 17:45:38 +0530

    Add: concatenation-of-array - submission-3

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-3.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun getConcatenation(nums: IntArray): IntArray {
+        val ans = IntArray(nums.size * 2)
+        for(i in 0 until nums.size){
+            ans[i] = nums[i]
+            ans[i + nums.size] = nums[i]
+        }
+        
+      
+
+        return ans
+    }
+}
